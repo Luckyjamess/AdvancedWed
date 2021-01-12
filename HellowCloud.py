4f959deb2eaa566e284a876da11a950313825bab
Luckyjamess <6106021630023@fitm.kmutnb.ac.th>
2021-01-12 11:18:34 +0700

    yess

--- HellowCloud.py
+++ HellowCloud.py
@@ -2,7 +2,7 @@ from flask import Flask
 server = Flask(__name__)
 
 def hello():
-    return "Hello from Luckyjames"
+    return "Hello Luckyjames : Happy New Years"
 
 if __name__ == "__main__":
     server.run(host='0.0.0.0', port=80)
