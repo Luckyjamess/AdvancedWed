0c8a402f4d285d4a319242ddd65bb0397644ed6f
Luckyjamess <6106021630023@fitm.kmutnb.ac.th>
2021-03-09 10:26:55 +0700

    test

--- HellowCloud.py
+++ HellowCloud.py
@@ -2,8 +2,8 @@ from flask import Flask
 server = Flask(__name__)
 
 @server.route("/")
-def hello():
-    return "Hello Luckyjames : Happy New Years road to 2022-2060"
+def hello():ß
+    return "Hello Luckyjames : Happy New Years road to 2022-3000s"
 
 if __name__ == "__main__":
     server.run(host='0.0.0.0', port=80)
